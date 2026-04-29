4197d3f3ba3b64a2d44d637ac7eecf3d213d6830
Gaganthakur0101 <thakurgagan0101@gmail.com>
2026-04-29 12:27:28 +0530

    fixing cloudinary

--- next.config.ts
+++ next.config.ts
@@ -6,10 +6,14 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
       },
     ],
   },
